e4662b4bb2d79a80ec3e9d83552de3ce6f54ce95
nomikazu <>
2021-06-14 22:25:42 +0900

    comment削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,10 @@ class CommentsController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    @comment = post.comments.build(comment_params.merge!(user_id: current_user.id))
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params.merge!(user_id: current_user.id))
     if @comment.save!
-      redirect_to post_path(post), notice: 'コメントできました'
+      redirect_to post_path(@post), notice: 'コメントできました'
     else
       flash.now[:error] = 'コメントできませんでした'
       render :new
@@ -18,7 +18,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy!
+    redirect_to post_path(@post)
   end
 
   private
