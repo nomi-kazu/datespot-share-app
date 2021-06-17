f934928ac8594a5bb111dfab750a667dddb4395b
nomikazu <>
2021-06-17 19:28:55 +0900

    kaminariでページネーション設定

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -4,5 +4,7 @@ class PrefecturesController < ApplicationController
     @posts = Post.where(prefecture: @params)
 
     @tag_lists = Tag.all.limit(3)
+
+    @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(5)
   end
 end
