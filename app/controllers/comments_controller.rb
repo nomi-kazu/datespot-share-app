db6e6161a79f6980f701ba32b5605252ca47e5dc
nomikazu <>
2021-06-20 15:18:21 +0900

    active_adminで管理画面作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
 
-  def index 
-    post = Post.find(params[:post_id])
-    comments = post.comments
-
-    render json: comments 
-  end
-
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
