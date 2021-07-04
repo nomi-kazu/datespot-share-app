5d8bd9400d20126c66ef17dd028fd7a215a4e462
nomikazu <>
2021-07-04 10:43:19 +0900

    fix: css

--- spec/systems/register_spec.rb
+++ spec/systems/register_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー登録' do
+RSpec.describe 'アカウント作成' do
 
-  it 'サインアップできる' do
+  it 'アカウント作成できる' do
     visit new_user_registration_path
     fill_in 'user[username]', with: 'samplename'
     fill_in 'user[email]', with: 'example@co.com'
     fill_in 'user[password]', with: 'aaaaaa'
     fill_in 'user[password_confirmation]', with: 'aaaaaa'
-    click_on 'サインアップ'
+    click_on 'アカウント作成'
     expect(page). to have_css('.post_index_title', text: '投稿一覧')
   end
 end
